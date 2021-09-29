2c9d8570fec5c6b6bb4bc5279403481122c83d6f
Ryan Chandler <ryangjchandler@gmail.com>
2021-09-29 23:40:13 +0100

    feature: List.reverse()

--- src/stdlib/list.rs
+++ src/stdlib/list.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use crate::environment::{Value, NativeMethodCallback};
 use crate::interpreter::Interpreter;
 
@@ -8,6 +10,7 @@ impl ListObject {
         match name.as_str() {
             "isEmpty" => list_is_empty,
             "isNotEmpty" => list_is_not_empty,
+            "reverse" => list_reverse,
             _ => panic!("Undefined method: {}", name),
         }
     }
@@ -20,7 +23,16 @@ fn list_is_empty(_: &mut Interpreter, context: Value, arguments: Vec<Value>) -> 
 }
 
 fn list_is_not_empty(_: &mut Interpreter, context: Value, arguments: Vec<Value>) -> Value {
-    super::arity("List.isEmpty()", 0, &arguments);
+    super::arity("List.isNotEmpty()", 0, &arguments);
 
     Value::Bool(! context.to_vec().borrow().is_empty())
+}
+
+fn list_reverse(_: &mut Interpreter, context: Value, arguments: Vec<Value>) -> Value {
+    super::arity("List.reverse()", 0, &arguments);
+
+    let rc = Rc::clone(&context.to_vec());
+    rc.borrow_mut().reverse();
+
+    Value::List(rc)
 }
